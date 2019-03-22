70999c2f5a11f2fefcf5e31d38b174ecf9f840f4
bertw2002 <awan00@stuy.edu>
2019-03-22 11:36:24 -0400

    finished tostring and constructors

--- MyDeque.java
+++ MyDeque.java
@@ -6,16 +6,44 @@ public class MyDeque<E>{
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[10];
     data = d;
+    start = -1;
+    size = 10;
+    end = -1;
   }
   public MyDeque(int initialCapacity){
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[initialCapacity];
     data = d;
+    size = initialCapacity
+    start = -1;
+    end = -1;
   }
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
+  public int size(){
+    return data.length;
+  }
+  public String toString(){
+    String str = "{ ";
+    for (int x = 0; x < size(); x++){
+      str += data[x];
+      str += " ";
+    }
+    return str;
+  }
+  public void addFirst(E element){
+    if (size() != 0){
+      if (end == -1){
+        data[0] = element;
+        start = 0;
+        end = 0;
+      }
+      else if (end != -1){
+
+      }
+    }
+  }
+  public void addLast(E element){
+
+  }
   public E removeFirst(){ }
   public E removeLast(){ }
   public E getFirst(){ }
