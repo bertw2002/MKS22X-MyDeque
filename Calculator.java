a33f022d0a0dd5aac69261b0aa31ff97fee966d7
bertw2002 <awan00@stuy.edu>
2019-03-25 11:50:28 -0400

    started eval

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,18 @@
+public class Calculator{
+  /*Evaluate a postfix expression stored in s.
+ *Assume valid postfix notation, separated by spaces.
+ */
+ public static double eval(String s){
+   MyDeque token = new MyDeque();
+   for (int x = 0; x < s.length(); x++){
+     while (!(s.substring(x, x + 1).equals(" "))){
+       token.addFirst(Integer.parseInt(s.substring(x, x + 1)));
+       x++;
+     }
+   }
 
+
+
+   return 0.0;
+ }
+}
