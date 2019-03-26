da3735c5675dee97dbdd7ca8cbfbc531233073c9
bertw2002 <awan00@stuy.edu>
2019-03-26 08:57:04 -0400

    wrote some code

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ public class Calculator{
  *Assume valid postfix notation, separated by spaces.
  */
  public static double eval(String s){
-   MyDeque token = new MyDeque();
+   MyDeque<String> token = new MyDeque<String>();
    for (int x = 0; x < s.length(); x++){
      while (!(s.substring(x, x + 1).equals(" "))){
        token.addFirst(Integer.parseInt(s.substring(x, x + 1)));
